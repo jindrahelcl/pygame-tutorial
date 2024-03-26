f3c229122b7ac7a45d6f8966129cbb1534c8b682
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2024-03-26 23:44:19 +0100

    animace!!!

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import sys
 import pygame
 
-from scripts.entities import PhysicsEntity
-from scripts.utils import load_image, load_images
+from scripts.entities import PhysicsEntity, Player
+from scripts.utils import load_image, load_images, Animation
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
 
@@ -28,10 +28,15 @@ class Game:
       "large_decor": load_images("tiles/large_decor"),
       "stone": load_images("tiles/stone"),
       "background": load_image("background.png"),
-      "clouds": load_images("clouds")
+      "clouds": load_images("clouds"),
+      "player/idle": Animation(load_images("entities/player/idle"), img_dur=6),
+      "player/run": Animation(load_images("entities/player/run"), img_dur=4),
+      "player/jump": Animation(load_images("entities/player/jump")),
+      "player/slide": Animation(load_images("entities/player/slide")),
+      "player/wall_slide": Animation(load_images("entities/player/wall_slide")),
     }
 
-    self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
+    self.player = Player(self, (50, 50), (8, 15))
     self.tilemap = Tilemap(self, tile_size=16)
     self.clouds = Clouds(self.assets["clouds"], count=16)
 
@@ -40,7 +45,6 @@ class Game:
   def run(self):
     while True:
       self.display.blit(self.assets["background"], (0, 0))
-
 
       self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 15
       self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 15
